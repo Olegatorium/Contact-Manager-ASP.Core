8a340edef717373798849f5547dfcd4cf37de7a2
Oleh <olegatorium@gmail.com>
2024-05-24 17:13:50 +0200

    Add integration test for index method in personController

---
+++ CRUDTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,32 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CRUDTests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+
+            builder.UseEnvironment("Test");
+
+            builder.ConfigureServices(services =>
+            {
+                var descripter = services.SingleOrDefault(t => t.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+                if (descripter != null) 
+                {
+                   services.Remove(descripter);
+                }
+                services.AddDbContext<ApplicationDbContext>(options => 
+                {
+                    options.UseInMemoryDatabase("DatabaseForTesting");
+                });
+            });
+        }
+    }
+}
---
+++ CRUDTests/PersonsControllerIntegrationTest.cs
@@ -0,0 +1,45 @@
+﻿using Fizzler.Systems.HtmlAgilityPack;
+using FluentAssertions;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDTests
+{
+    public class PersonsControllerIntegrationTest : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly HttpClient _client;
+
+        public PersonsControllerIntegrationTest(CustomWebApplicationFactory factory)
+        {
+            _client = factory.CreateClient(); 
+        }
+
+        #region Index
+        [Fact]
+        public async Task Index_ToReturnView() 
+        {
+            //Arrange
+
+            //Act
+            HttpResponseMessage response = await _client.GetAsync("/Persons/Index");
+
+            //Assert
+            response.Should().BeSuccessful();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            HtmlDocument html = new HtmlDocument();
+
+            html.LoadHtml(responseBody);
+
+            var document = html.DocumentNode;
+
+            document.QuerySelectorAll("table.persons").Should().NotBeNull();
+        }
+        #endregion
+    }
+}
